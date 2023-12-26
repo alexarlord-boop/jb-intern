7ddf3cca4dd141d30ad8573c93b7e55c028dca4d
alexarlord-boop <rezerw0.1@yandex.ru>
2023-12-26 11:10:14 +0200

    JB-INTERN-2024 Updated functions.js -- static file for further functionality

--- src/main/resources/mycontent/functions.js
+++ src/main/resources/mycontent/functions.js
@@ -3,3 +3,8 @@ function toggleVisibility(index) {
     var element = document.getElementById('commitMessage' + index);
     element.classList.toggle('hidden');
 }
+
+$('[id^="jobsModal"]').on('shown.bs.modal', function (e) {
+    console.log(`Modal with ID ${this.id} is about to be shown`);
+    // Your additional logic here
+});
