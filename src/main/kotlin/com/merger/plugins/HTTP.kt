5a83d977bc38e1cbd14eca3616f64f86e6c1e5d1
alexarlord-boop <rezerw0.1@yandex.ru>
2023-12-28 17:21:57 +0200

    JB-INTERN-2024 Updated task1.md content

--- src/main/kotlin/com/merger/plugins/HTTP.kt
+++ src/main/kotlin/com/merger/plugins/HTTP.kt
@@ -1,7 +1,9 @@
 package com.merger.plugins
 
 import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.plugins.cors.routing.*
 import io.ktor.server.plugins.defaultheaders.*
 
